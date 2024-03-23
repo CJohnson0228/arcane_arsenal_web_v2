cbd533a1c0535ed69bd0b74fb142e636759a283d
CJohnson0228 <68411376+CJohnson0228@users.noreply.github.com>
2024-03-23 14:20:46 -0400

    main app layout begin

--- client/src/pages/MainLayout.tsx
+++ client/src/pages/MainLayout.tsx
@@ -1,5 +1,31 @@
+import styled from '@emotion/styled'
+import LeftBar from '../components/MainLayout/LeftBar'
+import MainWindow from '../components/MainLayout/MainWindow'
+import RightBar from '../components/MainLayout/RightBar'
+import TopBar from '../components/MainLayout/TopBar'
+import { colors } from '../style/style.constants'
+
+const Container = styled.div({
+  position: 'relative',
+  height: '100vh',
+  width: '100vw',
+  overflow: 'hidden',
+  color: colors.text.light,
+  backgroundColor: colors.black,
+  backgroundImage: 'url("/images/background.png")',
+  backgroundSize: '1440px 1440px',
+  backgroundPosition: 'center',
+})
+
 const MainLayout = () => {
-  return <div>MainLayout</div>
+  return (
+    <Container className='content'>
+      <TopBar />
+      <RightBar />
+      <LeftBar />
+      <MainWindow />
+    </Container>
+  )
 }
 
 export default MainLayout
